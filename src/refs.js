cd5bca2d63e5a774d7100134c095f88a756e3279
Mozgarik <91159717+Mozgarik@users.noreply.github.com>
2023-05-10 22:36:43 +0300

    update

---
+++ src/refs.js
@@ -0,0 +1,7 @@
+const refs = {
+    input: document.querySelector("#search-box"),
+    countryList: document.querySelector(".country-list"),
+    countryInfo: document.querySelector(".country-info")
+}
+
+export default refs
